bba8346892e6d4fce955ca966e345c3459695c0e
Gani Ilham Irsyadi <ganiilhamirsyadi@gmail.com>
2021-04-28 10:32:02 +0700

    Initiate springboot project

---
+++ src/main/java/com/c2/sisteminformasitugas/SisteminformasitugasApplication.java
@@ -0,0 +1,13 @@
+package com.c2.sisteminformasitugas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisteminformasitugasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisteminformasitugasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/c2/sisteminformasitugas/SisteminformasitugasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c2.sisteminformasitugas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisteminformasitugasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
