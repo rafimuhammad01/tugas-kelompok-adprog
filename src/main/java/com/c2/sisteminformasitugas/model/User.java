b6378aa9e952cf24a04df61420cbeeaad76c7944
Gani Ilham Irsyadi <ganiilhamirsyadi@gmail.com>
2021-04-29 19:54:11 +0700

    Add model and repository

---
+++ src/main/java/com/c2/sisteminformasitugas/model/User.java
@@ -0,0 +1,30 @@
+package com.c2.sisteminformasitugas.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @Column(name = "npm", updatable = false, nullable = false, length = 10)
+    private String npm;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "is_admin")
+    private boolean isAdmin;
+}
