6710b9df9b30b75466727bfd7e5f714e10e22483
rafimuhammad01 <rafi10muhammad@gmai.com>
2021-04-30 14:01:33 +0700

    finished

---
+++ src/main/java/com/c2/sisteminformasitugas/model/DTO/ListKomentarDTO.java
@@ -0,0 +1,33 @@
+package com.c2.sisteminformasitugas.model.DTO;
+
+import com.c2.sisteminformasitugas.model.Komentar;
+
+public class ListKomentarDTO {
+    private int status;
+    private String message;
+    private Iterable<Komentar> data;
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Iterable<Komentar> getData() {
+        return data;
+    }
+
+    public void setData(Iterable<Komentar> data) {
+        this.data = data;
+    }
+}
---
+++ src/main/java/com/c2/sisteminformasitugas/model/Komentar.java
@@ -0,0 +1,28 @@
+package com.c2.sisteminformasitugas.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="komentar")
+@Data
+@NoArgsConstructor
+public class Komentar {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", updatable = false)
+    private int id;
+
+    @Column(name="comment")
+    private String comment;
+
+    /*
+    //TODO : Relation with User (Many to One) and Tugas (Many to One)
+
+    private User user;
+    private Tugas tugas;
+     */
+}
