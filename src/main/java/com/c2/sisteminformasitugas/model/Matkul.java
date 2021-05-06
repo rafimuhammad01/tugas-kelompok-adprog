d35960191d254a20948ce1cdbbefb090495d7f81
Gani Ilham Irsyadi <ganiilhamirsyadi@gmail.com>
2021-05-07 01:13:08 +0700

    Add relations between models

--- src/main/java/com/c2/sisteminformasitugas/model/Matkul.java
+++ src/main/java/com/c2/sisteminformasitugas/model/Matkul.java
@@ -1,25 +1,32 @@
 package com.c2.sisteminformasitugas.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "matkul")
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class Matkul {
     @Id
-    @Column(name = "kodeMatkul", updatable = false)
+    @Column(name = "kodeMatkul", updatable = false,  nullable = false)
     private String kodeMatkul;
 
     @Column(name = "nama")
     private String nama;
 
-    //OneToMany matkul-tugas
+    //Relationships One to Many (Tugas)
+    @OneToMany(mappedBy = "id", cascade = CascadeType.ALL)
+    private List<Tugas> tugas;
 
-    public Matkul(String kodeMatkul, String nama) {
-        this.kodeMatkul = kodeMatkul;
-        this.nama = nama;
-    }
+    //Relationships One to Many (Subscriber)
+    @ManyToMany(mappedBy = "matkulList", cascade = CascadeType.ALL)
+    private List<User> subscribers;
 }
--- src/main/java/com/c2/sisteminformasitugas/model/ToDoList.java
+++ src/main/java/com/c2/sisteminformasitugas/model/ToDoList.java
@@ -3,6 +3,8 @@ import com.fasterxml.jackson.annotation.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 import java.sql.Time;
@@ -12,6 +14,7 @@ import java.sql.Timestamp;
 @Entity
 @Table(name = "todolist")
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class ToDoList {
     @Id
@@ -24,4 +27,13 @@ public class ToDoList {
 
     @Column(name = "deadline", nullable = false)
     private Timestamp deadline;
+
+    @ManyToOne
+    @JoinColumn(name = "user_npm")
+    private User user;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "tugas_id")
+    private Tugas tugas;
+
 }
