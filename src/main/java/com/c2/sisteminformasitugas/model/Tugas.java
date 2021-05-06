eb16309dd76c4ee145f87083d57f543918cc27ad
Gani Ilham Irsyadi <ganiilhamirsyadi@gmail.com>
2021-05-06 21:38:18 +0700

    Fix relationship

--- src/main/java/com/c2/sisteminformasitugas/model/Tugas.java
+++ src/main/java/com/c2/sisteminformasitugas/model/Tugas.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 import java.sql.Time;
@@ -31,6 +33,11 @@ public class Tugas {
 
     @Column(name = "deadline", nullable = false)
     private Timestamp deadline;
+
+    @ManyToOne
+    @JoinColumn(name = "kodeMatkul")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Matkul matkul;
 
     //Relationships One to Many (Komentar), Many to One (Matkul)
     @OneToMany(mappedBy = "komentar")
