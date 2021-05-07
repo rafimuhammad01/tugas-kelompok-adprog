50dbbcd6e38d25cca1485bed026be3e89ccbf03f
ahmad.haydar <alkaf.haydar@gmail.com>
2021-05-07 20:21:00 +0700

    added required path kodeMatkul and matkulService to TugasController.java

--- src/main/java/com/c2/sisteminformasitugas/controller/TugasController.java
+++ src/main/java/com/c2/sisteminformasitugas/controller/TugasController.java
@@ -1,6 +1,7 @@
 package com.c2.sisteminformasitugas.controller;
 
 import com.c2.sisteminformasitugas.model.Tugas;
+import com.c2.sisteminformasitugas.service.MatkulService;
 import com.c2.sisteminformasitugas.service.TugasService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,15 +14,19 @@ public class TugasController {
     @Autowired
     private TugasService tugasService;
 
+    @Autowired
+    private MatkulService matkulService;
+
     @GetMapping(produces = {"application/json"})
     @ResponseBody
     public ResponseEntity<Iterable<Tugas>> getListTugas() {
         return ResponseEntity.ok(tugasService.getListTugas());
     }
 
-    @PostMapping(produces = {"application/json"})
+    @PostMapping(path = "/{kodeMatkul}", produces = {"application/json"})
     @ResponseBody
-    public ResponseEntity createTugas(@RequestBody Tugas tugas) {
+    public ResponseEntity createTugas(@RequestBody Tugas tugas, @PathVariable(value = "kodeMatkul") String kodeMatkul) {
+        tugas.setMatkul(matkulService.getMatkul(kodeMatkul));
         return ResponseEntity.ok(tugasService.createTugas(tugas));
     }
 
