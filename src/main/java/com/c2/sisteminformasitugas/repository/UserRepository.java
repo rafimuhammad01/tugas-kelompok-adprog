4a4612349123d7f4a58267520cb2862d7e3b75d0
Gani Ilham Irsyadi <ganiilhamirsyadi@gmail.com>
2021-05-07 19:07:53 +0700

    Add @Repository decorator for each repository interface

--- src/main/java/com/c2/sisteminformasitugas/repository/UserRepository.java
+++ src/main/java/com/c2/sisteminformasitugas/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.c2.sisteminformasitugas.repository;
 
 import com.c2.sisteminformasitugas.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
     User findByEmail(String email);
 }
