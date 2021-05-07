c693ebd7feb98ad4438a9767d80f69ed2ddbc789
ahmad.haydar <alkaf.haydar@gmail.com>
2021-05-07 23:00:12 +0700

    add a single test to tugasControllerTest.java

---
+++ src/test/java/com/c2/sisteminformasitugas/controller/TugasControllerTest.java
@@ -0,0 +1,111 @@
+package com.c2.sisteminformasitugas.controller;
+
+import com.c2.sisteminformasitugas.model.DTO.ListKomentarDTO;
+import com.c2.sisteminformasitugas.model.Komentar;
+import com.c2.sisteminformasitugas.model.Matkul;
+import com.c2.sisteminformasitugas.model.Tugas;
+import com.c2.sisteminformasitugas.model.User;
+import com.c2.sisteminformasitugas.security.constant.SecurityConstant;
+import com.c2.sisteminformasitugas.service.*;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.mockito.ArgumentMatchers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import java.sql.Timestamp;
+import java.util.Arrays;
+import java.util.Date;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class TugasControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private KomentarServiceImpl komentarService;
+
+    @MockBean
+    private TugasServiceImpl tugasService;
+
+    @MockBean
+    private UserServiceImpl userService;
+
+    private Komentar komentar;
+    private Tugas tugas;
+    private Matkul matkul;
+    private User user;
+    private String userJson;
+    private Tugas tugasPassedDeadline;
+
+    @BeforeEach
+    public void setUp() {
+        matkul = new Matkul();
+        matkul.setKodeMatkul("123");
+        matkul.setNama("Dummy");
+
+        tugas = new Tugas();
+        tugas.setId(1);
+        tugas.setJudul("Dummy");
+        tugas.setDeadline(Timestamp.valueOf("2022-09-01 09:01:15"));
+        tugas.setDeskripsi("Dummy");
+        tugas.setLink("Dummy");
+        tugas.setMatkul(matkul);
+        matkul.getTugas().add(tugas);
+        user = new User();
+        user.setNpm("1234");
+        user.setAdmin(false);
+        user.setEmail("test@gmail.com");
+        user.setPassword("test123");
+
+        userJson = "{\n" +
+                "    \"npm\": \"1906350616\",\n" +
+                "    \"email\": \"ganiilhamirsyadi@gmail.com\",\n" +
+                "    \"password\": \"password\"\n" +
+                "}";
+    }
+
+    private String getJWTToken() {
+        return Jwts.builder()
+                .setSubject(user.getEmail())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + SecurityConstant.EXPIRATION_TIME))
+                .signWith(SecurityConstant.HKEY)
+                .compact();
+    }
+
+    private String mapToJson(Object obj) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(obj);
+    }
+
+    @Test
+    void testGetListTugas() throws Exception {
+        Iterable<Tugas> listTugas = Arrays.asList(tugas);
+        when(tugasService.getTugas(tugas.getId())).thenReturn(tugas);
+        when(tugasService.getListTugas()).thenReturn(listTugas);
+
+        System.out.println(tugasService.getListTugas());
+        mvc.perform(get("/tugas/1").header("Authorization", "Bearer " +  getJWTToken())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk()).andExpect(content()
+                .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.data[1].id").value(tugas.getId()));
+    }
+
+}
